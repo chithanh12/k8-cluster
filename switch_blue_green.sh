694ab0f2563ba36e852c20d06252fdafb335389b
thanh.nguyen <thanh.nguyen@doctoranywhere.com>
2024-12-22 22:40:59 +0700

    update code

--- switch_blue_green.sh
+++ switch_blue_green.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 NEW_SLOT=$1
-OLD_SLOT='green'
+
 if [[ $NEW_SLOT == 'blue' ]]; then
 	echo "[INFO] Disable plugin as the new_slot is blue..."
 	echo "[INFO] Patching iam service to the new slot[blue] ..."
     kubectl -ndev scale --replicas 3 deployment dev-iam-blue
 	kubectl patch svc iam -n dev --type='json' -p='[{"op": "replace", "path": "/spec/selector/slot", "value":"blue"}]'
+    kubectl -ndev scale --replicas 0 deployment dev-iam-green
 else
-    OLD_SLOT='blue'
     kubectl -ndev scale --replicas 3 deployment dev-iam-green
 	echo "[INFO] Patching ${k8sServiceName} service to the new slot[green]..."
 	kubectl patch svc iam -n dev --type='json' -p='[{"op": "replace", "path": "/spec/selector/slot", "value":"green"}]'
+    kubectl -ndev scale --replicas 0 deployment dev-iam-blue
 fi
-
-kubectl -ndev scale --replicas 0 deployment dev-iam-${OLD_SLOT}
